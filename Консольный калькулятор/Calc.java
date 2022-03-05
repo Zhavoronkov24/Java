2ba6e45babc466c9e112da8c14e5b701c99649a1
Zhavoronkov-VA <Password0102>
2022-03-05 16:55:36 +0300

    Мелкие фиксы

---
+++ Консольный калькулятор/Calc.java
@@ -0,0 +1,47 @@
+package Dir1;
+
+import java.util.Scanner;
+
+public class Calc {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Выберите операцию: ");  //|\
+        System.out.println("1.Вычитание");          //| \
+        System.out.println("2.Сложение");           //|  > Типы операций
+        System.out.println("3.Умножение");          //| /
+        System.out.println("4.Деление");            //|/
+
+        int operation = scanner.nextInt();
+        System.out.println("Введите первое число - ");      // Число 1
+        int int1 = scanner.nextInt();
+        System.out.println("Введите второе число - ");      // Число 2
+        int int2 = scanner.nextInt();
+
+        int sum = int1 + int2;          // Сложение
+        int subs = int1 - int2;         // Вычитание
+        int multipl = int1 * int2;      // Умножение
+                                        //Деление в case 4
+
+        switch (operation){
+            case 1:
+                System.out.println("Результат : " + subs);
+                break;
+            case 2:
+                System.out.println("Результат : " + sum);
+                break;
+            case 3:
+                System.out.println("Результат : " + multipl);
+                break;
+            case 4: try {
+                System.out.println("Результат: " + int1 / int2);
+
+            } catch (ArithmeticException e){
+                System.out.println("На 0 делить нельзя!");
+            } break;
+
+            default:
+                System.out.println("Операция выбрана не корректно. Попробуйте снова");
+        }
+        System.out.println("Благодарю за использование калькулятора!");
+    }
+}
